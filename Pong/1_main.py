c357589670a3af5964d773c205711da3aad92273
Pranshu <pranshuchanana07@gmail.com>
2026-07-15 12:45:06 +0530

    Updated 1_main.py
    Added a dotted line below the scoreboard and adjusted paddle positions.

--- Pong/1_main.py
+++ Pong/1_main.py
@@ -10,10 +10,24 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, -50))
+l_paddle = Paddle((-350, -50))
 ball = Ball()
 scoreboard = Scoreboard()
+
+# Dotted-line below scoreboard
+line = Turtle()
+line.color("white")
+line.hideturtle()
+line.penup()
+line.goto(-400, 200)
+line.pendown()
+for _ in range(100):
+    line.forward(10)
+    line.penup()
+    line.forward(10)
+    line.pendown()
+line.penup()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -28,11 +42,11 @@ while game_is_on:
     ball.move()
 
     #Detect collision with wall
-    if ball.ycor() > 280 or ball.ycor() < -280:
+    if ball.ycor() > 190 or ball.ycor() < -280:
         ball.bounce_y()
 
     #Detect collision with paddle
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+    if ball.distance(r_paddle) < 60 and ball.xcor() > 320 or ball.distance(l_paddle) < 60 and ball.xcor() < -320:
         ball.bounce_x()
 
     #Detect R paddle misses
