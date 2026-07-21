f67ccd508a251f8a23d5ef72750492f6f64db2e9
Pranshu <pranshuchanana07@gmail.com>
2026-07-21 14:11:27 +0530

    Updated main file
    Refactored game loop into a separate function and added reset functionality on collision.

--- Snake game/main.py
+++ Snake game/main.py
@@ -23,27 +23,36 @@ screen.onkey(snake.turn_left, "w")
 screen.onkey(snake.turn_right, "s")
 
 screen.update()
+def game_play():
+    scoreboard.update_score()
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+        # Detect collision with food.
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
 
-    # Detect collision with food.
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-
-    # Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.display_game_over()
-
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
+        # Detect collision with wall.
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
             game_is_on = False
+            scoreboard.reset()
+            snake.reset()
             scoreboard.display_game_over()
 
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.reset()
+                snake.reset()
+                scoreboard.display_game_over()
+
+screen.onkey(game_play, "space")
+game_play()
 screen.exitonclick()
+
+
